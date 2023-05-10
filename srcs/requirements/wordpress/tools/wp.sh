e6ee651316beb2107f4970bc51dc25479676f228
Peannut <zakariaspam0@gmail.com>
2023-05-10 16:21:39 +0100

    finished I guess

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -5,6 +5,6 @@ wp config create --path=/var/www/html --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_U
 wp core install --path=/var/www/html --url=$WPR_URL --title=$WPR_TITLE --admin_user=$WPR_ADMIN_USER --admin_password=$WPR_ADMIN_PASS --admin_email=$WPR_ADMIN_EMAIL --allow-root
 wp user create --path=/var/www/html $WPR_USER $WPR_EMAIL --role=author --user_pass=$WPR_PASS --allow-root
 
-sed -i 's/listen = /run/php/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 php-fpm7.3 -F
